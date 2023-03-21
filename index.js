46ca3fda57d18226da0cfeb0fef189821fef9bc1
Cmma16 <114612202+Cmma16@users.noreply.github.com>
2023-03-21 13:28:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var operation = '';
+var result = document.getElementById("result")
+function addToResult(value) {
+    if(result.value === '' && value == '0')
+    {
+        //do nothing
+    }else{
+        result.value += value;
+    }
+}
+
+function addOperation(value) {
+    if(result.value != '')
+    {
+        if (operation) {
+            document.getElementById('result').value = document.getElementById('result').value.slice(0, -1) + value;
+        } else {
+            document.getElementById('result').value += value;
+        }
+        operation = value;
+    }
+}
+
+function clearResult() {
+    result.value = "";
+}
+  
+function calculateResult() {
+    if(result.value != '')
+    {
+        var result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+        operation = '';
+    }
+}
